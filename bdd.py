fae94995a8393a62a3f17bb3c30274b5d5e8339c
AzoulayDan <dan.azoulay@imerir.com>
2017-11-10 17:02:34 +0100

    Tentative de deploiement sur Heroku

---
+++ bdd.py
@@ -0,0 +1,15 @@
+#-*- encoding:utf-8 -*-
+from flask import Flask
+from flask_cors import CORS
+from db import Db()
+
+@app.route('/')
+def init_database():
+	db = Db()
+	db.executeFile('database.sql')
+	db.close()
+	return "Database initialised"
+
+if __name__ == '__main__':
+	app.run()
+
